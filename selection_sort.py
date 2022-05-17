fffaa964f8e6e80024f59c41cc22d27e3ceae2e9
Akshit Madan <akmadan1717@gmail.com>
2022-05-17 19:39:46 +0530

    codes

---
+++ selection_sort.py
@@ -0,0 +1,20 @@
+def selectionSort( itemsList ):
+    n = len( itemsList )
+    for i in range( n - 1 ): 
+        minValueIndex = i
+
+        for j in range( i + 1, n ):
+            if itemsList[j] < itemsList[minValueIndex] :
+                minValueIndex = j
+
+        if minValueIndex != i :
+            temp = itemsList[i]
+            itemsList[i] = itemsList[minValueIndex]
+            itemsList[minValueIndex] = temp
+
+    return itemsList
+
+
+lst = list(map(int, input('Enter numbers list: ').split()))
+
+print(selectionSort(lst))
